fa238446f0704d2a8da0384948131e13300c488b
ATHUL V <athulv054@gmail.com>
2025-02-10 16:08:44 +0530

    event create bug fix

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,18 +1,20 @@
+import EventForm from "@/components/shared/EventForm"
+import { auth } from "@clerk/nextjs/server";
+import { useEffect, useState } from "react";
 
-import React from 'react'
-
-export default function page() {
+const CreateEvent = async () => {
+  const { userId } = await auth();
   return (
     <>
-   <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py:10'>
-    <h3 className='wrapper h3-bold text-center  sm:text-left'>Create Event</h3>
-   </section>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+      </section>
 
-   <div className='wrapper my-8'>
-
-   </div>
-
-   </>
+      <div className="wrapper my-8">
+        <EventForm userId={userId || ''} type="Create" />
+      </div>
+    </>
   )
 }
 
+export default CreateEvent
